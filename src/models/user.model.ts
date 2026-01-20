a8a39a0b19195c4245a97e6b96908f32f9c275e5
abhpandey <abhishekpaney@gmail.com>
2026-01-20 10:19:16 +0545

    updated user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,7 @@ const UserSchema: Schema = new Schema<UserType>(
     {
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
+        name: { type: String, required: true, unique: true },
         firstName: { type: String },
         lastName: { type: String },
         role: {
